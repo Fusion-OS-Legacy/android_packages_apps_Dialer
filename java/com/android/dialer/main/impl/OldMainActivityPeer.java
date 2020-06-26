Wave-Project/packages_apps_Dialer

Dialer: Allow per-call account selection
Change-Id: Ieac27294f4069b2cff17bb39a0954f74320ad943

java/com/android/dialer/main/impl/OldMainActivityPeer.java
@@ -492,9 +492,8 @@ public class OldMainActivityPeer implements MainActivityPeer, FragmentUtilListen
                 snackbarDurationMillis)
             .setAction(
                 R.string.view_conversation,
-                v ->
-                    mainActivity.startActivity(
-                        IntentProvider.getSendSmsIntentProvider(number).getIntent(mainActivity)))
+                v -> mainActivity.startActivity(
+                    IntentProvider.getSendSmsIntentProvider(number).getClickIntent(mainActivity)))
             .setActionTextColor(
                 ContextCompat.getColor(mainActivity, R.color.dialer_snackbar_action_text_color))
             .show();

Fusion-OS-Legacy/android_packages_apps_Dialer

java/com/android/dialer/main/impl/OldMainActivityPeer.java
@@ -639,7 +639,7 @@ public class OldMainActivityPeer implements MainActivityPeer, FragmentUtilListen
                 R.string.view_conversation,
                 v ->
                     activity.startActivity(
-                        IntentProvider.getSendSmsIntentProvider(number).getIntent(activity)))
+                        IntentProvider.getSendSmsIntentProvider(number).getClickIntent(activity)))
             .setActionTextColor(
                 ContextCompat.getColor(activity, R.color.dialer_snackbar_action_text_color))
             .show();